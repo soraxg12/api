395b6fca7bebe98f606a033b153b05deb1644404
Felipe Alvarenga <59491238+soraxg12@users.noreply.github.com>
2024-05-17 11:47:09 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,33 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-let lastRequest = null;
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.use(express.json()); // Middleware para parsear JSON
-app.get('/', (req, res) => {
-    res.send("lastRequest");
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-// Rota para salvar o request
-app.post('/save-request', (req, res) => {
-  lastRequest = req.body;
-  res.send({
-    "type": "CREATE_CUSTOMER_SERVICE",
-    "departmentUUID": "08bd4c24-f360-4b75-9f6b-7a409ca2d53a",
-    "text": "Redirecionando para atendimento."
-    });
-});
-
-// Rota para retornar o último request salvo
-app.get('/get-last-request', (req, res) => {
-  if (lastRequest) {
-    res.send(lastRequest);
-  } else {
-    res.send({ message: 'Nenhum request salvo.' });
-  }
-});
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+module.exports = app;

7c28d0323cbfb21eb74faa1d55315a46697543bb
Felipe Alvarenga <59491238+soraxg12@users.noreply.github.com>
2024-05-17 11:41:17 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,6 +4,9 @@ const app = express();
 let lastRequest = null;
 
 app.use(express.json()); // Middleware para parsear JSON
+app.get('/', (req, res) => {
+    res.send("lastRequest");
+});
 
 // Rota para salvar o request
 app.post('/save-request', (req, res) => {
